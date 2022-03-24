a0fb086e8c5b418eb086b517be72eeece1f5f236
Steliostyl <52712681+Steliostyl@users.noreply.github.com>
2022-03-24 18:50:46 +0200

    v1.0 (without timer)

--- Excercise 1 - Traffic Lights/Excercise 1 - Traffic Lights/main.c
+++ Excercise 1 - Traffic Lights/Excercise 1 - Traffic Lights/main.c
@@ -3,36 +3,62 @@
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
-#define pin0 1	// Pin 0 = Big Road Cars
-#define pin1 2	// Pin 1 = Big Road Pedestrians
-#define pin2 4	// Pin 2 = Small Road
+#define pin0 1		// Pin 0 = Big Road
+#define pin1 2		// Pin 1 = Pedestrians
+#define pin2 4		// Pin 2 = Small Road
+#define sensTimer 0	// Time to wait before rechecking the sensor's data
+#define pedTimer 1	// Pedestrian crossing timer
 
-int interr = 0;
+int btf;		// Is 1 when big traffic light is on
+int sensorData;	/* Data from the small road's traffic sensor. 
+				When its value is 0, 5 or 8, it means there's 
+				traffic on the small road. */
 
 ISR(PORTF_PORT_vect){
 	int intflags = PORTF.INTFLAGS;
-	PORTF.INTFLAGS=intflags;
-	interr=1;
-	PORTD.OUTSET = pin0;
-	PORTD.OUTCLR = pin1;
+	PORTF.INTFLAGS = intflags;
+	
+	btf = PORTD.OUT % 2;	// Save current status of the Big Road's traffic lights
+
+	if (btf){	// Check if the Big Road's traffic light is on
+		PORTD.OUT = pin0;		// Turn off the Big Road's traffic light
+		PORTD.OUTCLR = pin2;	// Turn on the Small Road's traffic light
+	}
+
+	PORTD.OUTCLR = pin1;		// Turn on the Pedestrian traffic light
+	_delay_ms(pedTimer);		// Wait a few seconds before turning off the pedestrian's traffic light
+	PORTD.OUTSET = pin1;		// Turn off the Pedestrian traffic light
+	
+	if (btf){	// If the Big Road's traffic light was on before the interrupt
+		PORTD.OUTSET = pin2; // Turn off the Small Road's traffic light
+		PORTD.OUTCLR = pin0; // Turn on the Big Road's traffic light
+	}
 }
 
 int main(void)
-{
+{	
 	PORTD.DIR |=  7;	// Pin 0,1,2 = Output
-	PORTD.OUT |= 6;		// Initialize traffic lights (Only 0 is on)
-	int tmp = rand()%10;
+	PORTD.OUT |= 6;		// Initialize traffic lights (Only the Big Road's traffic light is on)
+	// Pull-up enable and Interrupt enabled with sense on both edges
+	PORTF.PIN5CTRL |= PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc;
+	sei();	// Enable interrupts
 	while (1){
-		while ( (tmp == 0) | (tmp == 5) | (tmp == 8) ){
+		sensorData = rand()%10;	// Change the sensor's data
+		// If there's traffic on the small road
+		if ( (sensorData == 0) || (sensorData == 5) || (sensorData == 8) ){
 			PORTD.OUTSET = pin0; // Turn off the Big Road's traffic light
 			PORTD.OUTCLR = pin2; // Turn on the Small Road's traffic light
-			_delay_ms(1);
-			tmp = rand()%10;
-		}
-		if (interr == 0){
-			PORTD.OUTSET = pin2; // Turn off the Small Road's traffic light
-			PORTD.OUTCLR = pin0; // Turn on the Big Road's traffic light
-			tmp = rand()%10;
+			do {
+				_delay_ms(sensTimer);	// Wait before reading the sensor's data
+				sensorData = rand()%10;	// Change the sensor's data
+			} while((sensorData == 0) || (sensorData == 5) || (sensorData == 8));
+			// ^ While there's traffic on the Small Road ^
+			
+			PORTD.OUTSET = pin2;	// Turn off the Small Road's traffic light
+			PORTD.OUTCLR = pin0;	// Turn on the Big Road's traffic light
+			_delay_ms(sensTimer);	// Wait before reading the sensor's data
 		}
 	}
+	cli();	// Disable interrupts (useless because we're using while(1))
 }
+
